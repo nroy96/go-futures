e7f26fbb8f046b2b9c1f097f97e58b5d51f834b0
Nishant Roy <nroy@gatech.edu>
2020-04-21 23:04:13 -0700

    Provide generic interface for futures

--- main.go
+++ main.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"fmt"
-	"go-futures/src"
+	"go-futures/futures"
 	"time"
 )
 
 func slowIntegerFetch() int {
-	time.Sleep(5 * time.Second)
+	time.Sleep(1 * time.Second)
 	return 5
+}
+
+func slowStringFetch(d time.Duration) string {
+	time.Sleep(d)
+	return "X"
 }
 
 func main() {
 	now := time.Now()
-	intFuture := futures.CreateIntFuture(slowIntegerFetch)
-	x, err := intFuture.GetWithTimeout(2*time.Second)
-	fmt.Println(x)
-	fmt.Println(err)
-	fmt.Printf("%v\n", time.Since(now))
+	f := futures.NewFuture(slowIntegerFetch)
+	x, err := f.Get()
+	fmt.Printf("value: %v\n", x)
+	fmt.Printf("error: %v\n", err)
+	fmt.Printf("Time taken to get value the first time: %v\n", time.Since(now))
+	
+	now = time.Now()
+	x, err = f.Get()
+	fmt.Printf("value: %v\n", x)
+	fmt.Printf("error: %v\n", err)
+	fmt.Printf("Time taken to get value the second time: %v\n", time.Since(now))
+
+	now = time.Now()
+	f = futures.NewFuture(slowStringFetch, 3*time.Second)
+	x, err = f.GetWithTimeout(1 * time.Second)
+	fmt.Printf("value: %v\n", x)
+	fmt.Printf("error: %v\n", err)
+	fmt.Printf("Time taken to get value with timeout the first time: %v\n", time.Since(now))
+
+	now = time.Now()
+	x, err = f.GetWithTimeout(5 * time.Second)
+	fmt.Printf("value: %v\n", x)
+	fmt.Printf("error: %v\n", err)
+	fmt.Printf("Time taken to get value with timeout the first time: %v\n", time.Since(now))
 }
